72e2fa5375040f48e6e433ea0b223abecbaf8e13
SeanR <seanrobertblitzntonya@gmail.com>
2025-02-04 22:09:46 +0000

    Added more automation

--- sensehat_publisher.py
+++ sensehat_publisher.py
@@ -18,16 +18,22 @@ CERTIFICATE_PATH2 = os.getenv("CERTIFICATE_PATH2")
 PRIVATE_KEY_PATH2 = os.getenv("PRIVATE_KEY_PATH2")
 ROOT_CA_PATH2 = os.getenv("ROOT_CA_PATH2")
 
+logging.basicConfig(level=logging.INFO,format="%(asctime)s - %(levelname)s - %(message)s")
 mqtt_client = AWSIoTMQTTClient(THING_NAME2)
 mqtt_client.configureEndpoint(IOT_ENDPOINT2, 8883)
 mqtt_client.configureCredentials(ROOT_CA_PATH2, PRIVATE_KEY_PATH2, CERTIFICATE_PATH2)
 
+#Enables auto reconnect in case of disconnection
+mqtt_client.configureAutoReconnectBackoffTime(1, 32, 20)
+mqtt_client.configureOfflinePublishQueueing(-1)
+mqtt_client.configureConnectDisconnectTimeout(10)
+mqtt_client.configureMQTTOperationTimeout(5)
 try:
     mqtt_client.connect()
     logging.info("Connected to AWS IoT CORE")
     
 except Exception as e: 
-    logging.error(f"Failed to connect to AWS IoT Core")
+    logging.error(f"Failed to connect to AWS IoT Core: {str(e)}")
     exit(1)
 
 sensor = SenseHat()
@@ -50,20 +56,34 @@ def read_sensor_data():
         current_time = int(time.time())
         ttl_time = current_time + (7 * 24 * 60 * 60)
         
-        return {
+        sensor_data = {
             "device_id": THING_NAME2,
             "timestamp": datetime.now(timezone.utc).isoformat(),
             "temperature": round(temperature, 2),
             "humidity": round(humidity, 2),
             "pressure": round(pressure, 2),
             "imu": {
-                "acceleration": {k: round (v, 2) for k,v in imu["acceleration"].items()},
-                "gyroscope": {k: round(v, 2) for k,v in imu["acceleration"].items()},
-                "magnetometer": {k: round(v, 2) for k,v in imu["magnetometer"].items()}
+                "acceleration": {
+                    "x": round(imu["acceleration"]["x"], 2),
+                    "y": round(imu["acceleration"]["y"], 2),
+                    "z": round(imu["acceleration"]["z"], 2),
+                },
+                "agyroscope": {
+                    "x": round(imu["gyroscope"]["x"], 2),
+                    "y": round(imu["gyroscope"]["y"], 2),
+                    "z": round(imu["gyroscope"]["z"], 2),
+                },
+                "magnetometer": {
+                    "x": round(imu["magnetometer"]["x"], 2),
+                    "y": round(imu["magnetometer"]["y"], 2),
+                    "z": round(imu["magnetometer"]["z"], 2),
+                }, 
             },
             "ttl_timestamp": ttl_time
         }
         
+        return sensor_data
+    
     except Exception as e:
         logging.error(f"Error reading sensor data: {str(e)}")
         return None
@@ -72,9 +92,11 @@ try:
     while True:
         sensor_data = read_sensor_data()
         if sensor_data:
-            mqtt_client.publish(IOT_TOPIC2, json.dumps(sensor_data), 1)
-            logging.info(f"Published to AWS IoT: {sensor_data}")
+            payload = json.dumps(sensor_data)
+            mqtt_client.publish(IOT_TOPIC2, payload, 1)
+            logging.info(f"Published to AWS IoT: {payload}")
         time.sleep(5)
+        
 except KeyboardInterrupt:
     logging.info("Stopping publishing")
 except Exception as e:
